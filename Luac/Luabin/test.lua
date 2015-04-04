ae5288a033eb542f7f56b3ab7e8e3189503d3494
KORYUOH <coyamasss@yahoo.co.jp>
2015-04-04 17:04:25 +0900

    add module

---
+++ Luac/Luabin/test.lua
@@ -0,0 +1,27 @@
+-- sample.lua
+--------------------------------------------------------------------------------
+
+Name = "KORYUOH"
+
+print("Hello, " .. Name .. "!")
+
+print("CallTest")
+
+Shout()
+print (add(5,6))
+
+function say()
+	print ("He Saied " ..Name.. "")
+end
+
+
+
+function speech()
+    print("Is your name " .. Name .. "?")
+end
+
+function sum( x , y )
+	print( ..x.. " + " ..y.. " = " ..x+y..)
+end
+
+Destroyer()
